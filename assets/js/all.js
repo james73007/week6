d5c97830c19a08323f0a807019e3a6c6471c21e1
unknown <roger73007@gmail.com>
2021-08-12 16:17:13 +0800

    Update 2021-08-12T08:17:08.408Z

---
+++ assets/js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // <!-- Initialize Swiper -->
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30 // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+});
+//# sourceMappingURL=all.js.map
