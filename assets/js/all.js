05985d550738afb25b9aa2d497660d309d8efabb
unknown <roger73007@gmail.com>
2021-08-14 17:00:26 +0800

    Update 2021-08-14T09:00:22.345Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,7 +4,13 @@ $(function () {
   console.log('Hello Bootstrap5');
 });
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
