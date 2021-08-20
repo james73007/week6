9650e30a1acff03e5869cb2fdd4597463d8b3e78
unknown <roger73007@gmail.com>
2021-08-20 12:32:27 +0800

    Update 2021-08-20T04:32:25.049Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,16 +1,21 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   breakpoints: {
     768: {
+      slidesPerView: 2
+    },
+    992: {
       slidesPerView: 3,
       spaceBetween: 30
     }
+  },
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false
   }
 });
 //# sourceMappingURL=all.js.map
